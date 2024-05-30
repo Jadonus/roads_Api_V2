63b850687534fa54767678190a7da07147887ecf
jadonus <jadongearhart@gmail.com>
2024-05-30 15:56:57 -0400

    DONE

--- api/app.py
+++ api/app.py
@@ -34,7 +34,7 @@ def delete_road():
     data = request.json
     userid = data.get("userid")
     road = data.get("roadtodelete")
-    db.delete_road(road=road, userid=userid)
+    db.delete_road(road=road, userid=userid.replace('"', ''))
     return "Done"
 
 
